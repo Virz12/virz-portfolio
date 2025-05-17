194884ac1b80a449927a7252d642c1ece50eda07
Virz <virgiriyadi2404@gmail.com>
2025-05-17 12:31:14 +0700

    fix: email send error add:  Analytics and SpeedInsights for Vercel

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,4 +1,11 @@
+<script setup>
+import { SpeedInsights } from "@vercel/speed-insights/nuxt"
+import { Analytics } from '@vercel/analytics/nuxt';
+</script>
+
 <template>
+  <Analytics />
+  <SpeedInsights />
   <div class="font-Dm-Sans">
     <NavBar />
     <slot></slot>
