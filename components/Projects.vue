da5e2bfce0a7eeb886b33feedad1fcba586ac249
Virz <108918835+Virz12@users.noreply.github.com>
2025-08-10 13:06:36 +0700

    feat: add everyvent project and delete whatsappIntegration project

--- components/Projects.vue
+++ components/Projects.vue
@@ -1,9 +1,9 @@
 <script setup>
 const projects = [
   {
-    title: 'WhatsApp Channel Integration (On Progress)',
-    image: '/images/whatsappIntegration.webp',
-    link: ''
+    title: 'Everyvent',
+    image: '/images/everyvent.webp',
+    link: 'https://everyvent.vercel.app'
   },
 ]
 </script>
@@ -20,14 +20,16 @@ const projects = [
         v-gsap.delay-300.whenVisible.once.reversible.stagger.from='{ opacity: 0, y: 50 }'>
         <div v-for="(item, index) in projects" :key="index"
           class="border rounded-xl relative cursor-pointer group basis-1/3 after:content-[''] after:absolute after:top-0 after:left-0 after:w-full after:h-full after:rounded-xl after:bg-black after:opacity-0 after:duration-300 hover:after:opacity-15">
-          <NuxtImg :src="item.image" size="100%" class="rounded-xl group-hover:blur-[2px]"
-            :alt="'Project ' + item.title">
-          </NuxtImg>
-          <div class="absolute z-10 bottom-0 w-full p-4">
-            <h3
-              class="text-2xl text-white font-black text-shadow-sm transition-300 -my-2.5 uppercase opacity-0 invisible group-hover:m-0 group-hover:opacity-100 group-hover:visible">
-              {{ item.title }}</h3>
-          </div>
+          <NuxtLink :to="item.link">
+            <NuxtImg :src="item.image" size="100%" class="rounded-xl group-hover:blur-[2px]"
+              :alt="'Project ' + item.title">
+            </NuxtImg>
+            <div class="absolute z-10 bottom-0 w-full p-4">
+              <h3
+                class="text-2xl text-white font-black text-shadow-sm transition-300 -my-2.5 uppercase opacity-0 invisible group-hover:m-0 group-hover:opacity-100 group-hover:visible">
+                {{ item.title }}</h3>
+            </div>
+          </NuxtLink>
         </div>
       </div>
       <div class="border-b border-primary pb-20"></div>
